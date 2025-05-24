8a835c38b0e40730ed66096962797170ba523045
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-05-24 12:07:00 +0100

    created reusable back button component

---
+++ components/BackButton.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
+
+export default function BackBtn() {
+  const router = useRouter();
+
+  const goBack = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
+
+  return (
+    <button
+      onClick={goBack}
+      aria-label="Go back"
+      className="flex items-center space-x-1 text-yellow-600 hover:text-yellow-800"
+    >
+      <ArrowLeft size={20} className="text-yellow-600 hover:text-yellow-800" />
+      <span>Go Back</span>
+    </button>
+  );
+}
