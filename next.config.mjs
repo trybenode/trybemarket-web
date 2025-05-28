72d3bb0dc13e4da22212816940ff6616843d2149
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-05-28 16:24:13 +0100

    install payStack dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+   dev: {
+    allowedDevOrigins: ['http://192.168.8.101:3000'], // adjust port as needed
+  },
 }
 
 export default nextConfig
