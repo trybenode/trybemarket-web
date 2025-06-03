2b7d6b657b2cc0b5197b7e86283226249e8c1401
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-06-03 05:42:58 +0100

    implemented product filter based on university

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,11 @@ import {
   orderBy,
   startAfter,
   limit,
+  where,
 } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 import ListingCardSkeleton from "@/components/ui/ListingCardSkeleton";
+import useUserStore from "@/lib/userStore";
 
 const ListingCards = dynamic(() => import("@/components/ListingCards"), {
   loading: () => (
@@ -33,7 +35,6 @@ const UserProfile = dynamic(() => import("@/components/UserProfile"), {
   ssr: false,
 });
 
-
 const PAGE_SIZE = 8;
 
 export default function HomePage() {
@@ -45,24 +46,39 @@ export default function HomePage() {
   const [lastVisible, setLastVisible] = useState(null);
   const [hasSearchQuery, setHasSearchQuery] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [hasMore, setHasMore] = useState(true); //used to stop endless loading when few products are uploaded
+  const selectedUniversity = useUserStore((state) => state.selectedUniversity);
+  const isUserStoreReady = useUserStore((state) => state.isInitialized);
+  const [shuffledProducts, setShuffledProducts] = useState([]);
 
   const fetchProducts = async (isLoadMore = false, isRefresh = false) => {
+    if (!isUserStoreReady) return; // wait for user store to initailize
     try {
       if (isLoadMore) setIsFetchingMore(true);
       else if (isRefresh) setRefreshing(true);
       else setLoading(true);
 
       const baseQuery = collection(db, "products");
-      let q = query(baseQuery, orderBy("createdAt", "desc"), limit(PAGE_SIZE));
+
+      let constraints = [orderBy("createdAt", "desc"), limit(PAGE_SIZE)];
+      console.log("Selected university:", selectedUniversity);
+      if (selectedUniversity) {
+        constraints.unshift(where("university", "==", selectedUniversity));
+      }
+
+      // let q = query(baseQuery, orderBy("createdAt", "desc"), limit(PAGE_SIZE));
 
       if (isLoadMore && lastVisible) {
-        q = query(
-          baseQuery,
-          orderBy("createdAt", "desc"),
-          startAfter(lastVisible),
-          limit(PAGE_SIZE)
-        );
+        constraints.push(startAfter(lastVisible));
+        // q = query(
+        //   baseQuery,
+        //   orderBy("createdAt", "desc"),
+        //   startAfter(lastVisible),
+        //   limit(PAGE_SIZE)
+        // );
       }
+
+      const q = query(baseQuery, ...constraints);
 
       const snap = await getDocs(q);
       const newProducts = snap.docs.map((doc) => ({
@@ -77,6 +93,10 @@ export default function HomePage() {
       // update lastVisible
       const lastDoc = snap.docs[snap.docs.length - 1] || null;
       setLastVisible(lastDoc);
+
+      if (snap.docs.length < PAGE_SIZE) setHasMore(false);
+      else setHasMore(true);
+      if (isLoadMore) setHasMore(true);
 
       setProducts((prev) => {
         if (!isLoadMore) return newProducts;
@@ -94,9 +114,25 @@ export default function HomePage() {
     }
   };
 
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
+
+  // const onRefresh = () => {
+  //   setHasSearchQuery(false); // Reset search query to show all products
+  //   setFilteredProducts([]); // Clear filtered products
+  //   setLastVisible(null); // Reset pagination
+  //   fetchProducts(false, true); // Fetch fresh data
+  // };
+
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    if (isUserStoreReady) {
+      setLastVisible(null); // Reset pagination
+      setHasMore(true);
+      fetchProducts(false, true); // Fetch fresh data
+    }
+    // fetchProducts();
+  }, [selectedUniversity, isUserStoreReady]);
 
   const onRefresh = () => {
     setHasSearchQuery(false); // Reset search query to show all products
@@ -105,22 +141,40 @@ export default function HomePage() {
     fetchProducts(false, true); // Fetch fresh data
   };
 
-  const productsToDisplay = hasSearchQuery ? filteredProducts : products;
+  //  Fisher-Yates algorithm
+  function shuffleArray(array) {
+    const arr = [...array];
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  useEffect(() => {
+    if (!hasSearchQuery && products.length > 0) {
+      setShuffledProducts(shuffleArray(products));
+    }
+  }, [products, hasSearchQuery]);
+  // const productsToDisplay = hasSearchQuery ? filteredProducts : products;
+  const productsToDisplay = hasSearchQuery
+    ? filteredProducts
+    : shuffledProducts;
 
   return (
-    <div className='flex flex-col min-h-screen max-w-6xl bg-white mx-auto'>
-      <div className='flex items-center justify-between py-4 px-4 mb-6'>
+    <div className="flex flex-col min-h-screen max-w-6xl bg-white mx-auto">
+      <div className="flex items-center justify-between py-4 px-4 mb-6">
         <Image
-          src='/assets/logo.png?height=50&width=150'
-          alt='Logo'
+          src="/assets/logo.png?height=50&width=150"
+          alt="Logo"
           width={150}
           height={40}
-          className='object-contain'
+          className="object-contain"
         />
         <UserProfile />
       </div>
 
-      <div className='flex-1 px-3'>
+      <div className="flex-1 px-3">
         <SearchBar
           onResults={(results, isSearchActive) => {
             setFilteredProducts(results);
@@ -134,23 +188,28 @@ export default function HomePage() {
             isLoading={loading}
             isFetchingMore={isFetchingMore}
             loadMoreProducts={() => {
-              if (!hasSearchQuery) fetchProducts(true);
+              if (!hasSearchQuery && hasMore) fetchProducts(true);
             }}
             refreshControl={onRefresh}
             refreshing={refreshing}
           />
         ) : (
-          <div className='flex flex-col items-center justify-center h-[50vh]'>
+          <div className="flex flex-col items-center justify-center h-[50vh]">
             <button
               onClick={onRefresh}
-              className='mb-4 text-blue-500 hover:underline'
+              className="mb-4 text-blue-500 hover:underline"
             >
               Refresh
             </button>
-            <p className='mx-4 text-center text-lg text-red-500'>
-              {hasSearchQuery
+            <p className="mx-4 text-center text-lg text-red-500">
+              {/* {hasSearchQuery
                 ? "No products found"
-                : "Please check your internet connection and refresh"}
+                : "Please check your internet connection and refresh"} */}
+              {hasSearchQuery
+                ? "No products found for this search."
+                : selectedUniversity
+                ? `No products in ${selectedUniversity} yet. Be the first to upload!`
+                : "No products found. Please check your internet connection or try again."}
             </p>
           </div>
         )}
