9d9cbad7b355911df5e8f6c2f5cb7f96eea69474
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-06-14 06:46:48 +0100

    implemented image compress logic

---
+++ utils/compressImage.js
@@ -0,0 +1,16 @@
+import imageCompression from "browser-image-compression";
+
+export async function compressImage(file) {
+  const options = {
+    maxSizeMB: 0.5,
+    maxWidthOrHeight: 1024,
+    useWebWorker: true,
+  };
+  try {
+    const compressed = await imageCompression(file, options);
+    return compressed;
+  } catch (error) {
+    console.error("Image compression error", error);
+    throw error;
+  }
+}
